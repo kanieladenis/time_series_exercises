b648408b3790677d87a24ff6f6484d43067db468
kanieladenis <kaniela.denis@gmail.com>
2022-01-11 17:01:04 -0600

    updated exercises

--- acquire.py
+++ acquire.py
@@ -5,12 +5,11 @@ import numpy as np
 
 def get_api_data(endpoint):
     """
-    Function will pull data from API interface in a while loop
+    Function will pull data from API interface in a while loop to get stores, items, and sales data
     """
     host = "https://python.zgulde.net/"
     api = "api/v1/"
-    endpoint = 'items'
-
+    
     url = host + api + endpoint
 
     response = requests.get(url).json()
@@ -40,3 +39,13 @@ def get_api_data(endpoint):
         next_page = payload['next_page']
         
     return df
+
+
+def stores_sales_items(sales, stores, items):
+    stores_sales = pd.merge(sales, stores, how='left', left_on='store', right_on='store_id')
+    stores_sales.head()
+
+    stores_sales_items = pd.merge(stores_sales, items, how='left', left_on='item', right_on='item_id')
+    stores_sales_items.head()
+    
+    return stores_sales_items
