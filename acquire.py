04f781a24e13556daf3db9dffd165264f8df1f42
kanieladenis <kaniela.denis@gmail.com>
2022-01-13 09:06:09 -0600

    updated acquire.py, aquire exercises, and prepare notebook

--- acquire.py
+++ acquire.py
@@ -3,13 +3,17 @@ import pandas as pd
 import numpy as np
 
 
+
+
+
+
 def get_api_data(endpoint):
     """
-    Function will pull data from API interface in a while loop to get stores, items, and sales data
+    Function will pull data from API interface in a while loop
     """
     host = "https://python.zgulde.net/"
     api = "api/v1/"
-    
+
     url = host + api + endpoint
 
     response = requests.get(url).json()
@@ -32,20 +36,53 @@ def get_api_data(endpoint):
 
         contents = pd.DataFrame(payload[endpoint])
 
-        df = pd.concat([df, contents])
-
-        df = df.reset_index(drop=True)
+        df = pd.concat([df, contents]).reset_index(drop=True)
 
         next_page = payload['next_page']
         
     return df
 
 
-def stores_sales_items(sales, stores, items):
-    stores_sales = pd.merge(sales, stores, how='left', left_on='store', right_on='store_id')
-    stores_sales.head()
+def csv_files():
+    import os
+    if (os.path.isfile('items.csv') == False) or (os.path.isfile('sales.csv')==False) or (os.path.isfile('stores.csv')==False):
+        items, stores, sales = stores_sales()
+        items.to_csv('items.csv')
+        stores.to_csv('stores.csv')
+        sales.to_csv('sales.csv')
+    else:
+        items = pd.read_csv('items.csv')
+        stores = pd.read_csv('stores.csv')
+        sales = pd.read_csv('sales.csv')
+        
+    return items, stores, sales
+        
+        
 
-    stores_sales_items = pd.merge(stores_sales, items, how='left', left_on='item', right_on='item_id')
-    stores_sales_items.head()
+def stores_sales():
     
-    return stores_sales_items
+    items = get_api_data(endpoint="items")
+    
+    stores = get_api_data(endpoint='stores')
+    
+    sales = get_api_data(endpoint='sales')
+    
+       
+    return items, stores, sales
+
+
+
+
+        
+        
+def sales_merged():
+    
+    items, stores, sales = csv_files()
+    
+    stores_sales = pd.merge(sales, stores, how='left', left_on='store', right_on='store_id')
+    
+    sales_merged = pd.merge(stores_sales, items, how='left', left_on='item', right_on='item_id')
+    
+    return items, stores, sales, sales_merged
+    
+    
